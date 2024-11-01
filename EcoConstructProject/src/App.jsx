fd7d5971863d0fde34b6eedd12424e6a0eeaebc7
unknown <kevinnaufal239@gmail.com>
2024-11-01 17:27:28 +0700

    Adding project folder to repository

---
+++ EcoConstructProject/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+const name = "World";
+
+return (
+    <>
+      <div className="">
+        <h1 className="text-4xl ">Hello {name}</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
